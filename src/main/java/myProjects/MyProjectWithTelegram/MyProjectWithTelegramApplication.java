31a72e027ebfc532534051f9ffc8dfb50be4d1b2
Andrew Zola <and.zola.77@gmail.com>
2023-10-09 16:36:30 +0200

    Initial commit

---
+++ src/main/java/myProjects/MyProjectWithTelegram/MyProjectWithTelegramApplication.java
@@ -0,0 +1,13 @@
+package myProjects.MyProjectWithTelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectWithTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectWithTelegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myProjects/MyProjectWithTelegram/MyProjectWithTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package myProjects.MyProjectWithTelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectWithTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
